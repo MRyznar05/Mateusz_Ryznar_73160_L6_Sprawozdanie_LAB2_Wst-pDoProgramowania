5bc9aea091de48ee70390b12fa3ee75b1b806a65
MRyznar05 <73160@student.wsiz.edu.pl>
2025-11-13 03:02:03 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -2,7 +2,7 @@ punkty = float(input("Podaj punkty: "))
 
 if punkty > 80:
     print("Zaliczyłeś egzamin, gratuluję!")
-elif punkty >= 50 and punkty < 80:
+elif punkty >= 50 and punkty <= 80:
     print("Zaliczyłeś egzamin, ale możesz poprawić jego wynik!")
 else:
     print("Nie zaliczyłeś egzaminu, pozdro na poprawce!")
